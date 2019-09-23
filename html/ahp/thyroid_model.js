3aacd2e52cc57769201a70e0498a3c489d4437f3
Bill Adams <wjladams@gmail.com>
2019-09-23 19:41:32 -0400

    Editing Natalie's json model.

--- html/ahp/thyroid_model.js
+++ html/ahp/thyroid_model.js
@@ -7,38 +7,34 @@ AHP_MODEL_JSON = {
       {
         "name": "Certain about cancer",
         "description": "Making sure I know if I have cancer",
-        "alt_scores": [0, 0.2, 1.0]
+        "alt_scores": [0, 0.8, 1.0]
       },
       {
-        "name": "No scar",
-        "description" : "Not having a scar",
-        "alt_scores" : [1.0, 0.2, 0.1]
-      },
-      {
-        "name": "Medication forever",
-        "description" : "The possible need to take medication forever",
-        "alt_scores" : [1.0, 0.2, 0.1]
-      },
-      {
-        "name": "Doctor rec",
-        "description" : "Whatever my doctor thinks I should do",
-        "alt_scores" : [1.0, 0.2, 0.1]
-      },
-      {
-        "name": "Avoid surgery",
-        "description" : "Not having surgery if I don't have to",
-        "alt_scores" : [1.0, 0.2, 0.1]
-      },
-      {
-        "name": "Risk to voice",
-        "description" : "Avoiding risks to my voice at all costs",
-        "alt_scores" : [1.0, 0.2, 0.1]
-      },
-      {
-        "name": "Other health issues",
-        "description" : "Concern related to other health problems",
-        "alt_scores" : [1.0, 0.2, 0.1]
-      },
+        "name" : "Surgerical concerns",
+        "description" : "Health concerns around various complications",
+        "children" : [
+          {
+            "name": "No scar",
+            "description" : "Not having a scar",
+            "alt_scores" : [1.0, 0.2, 0.1]
+          },
+          {
+            "name": "Medication forever",
+            "description" : "The possible need to take medication forever",
+            "alt_scores" : [1.0, 0.3, 0.1]
+          },
+          {
+            "name": "Avoid surgery",
+            "description" : "Not having surgery if I don't have to",
+            "alt_scores" : [1.0, 0.2, 0.1]
+          },
+          {
+            "name": "Risk to voice",
+            "description" : "Avoiding risks to my voice at all costs",
+            "alt_scores" : [1.0, 0.9, 0.5]
+          },
+        ]
+      }
   ],
 
   "pairwise": [
