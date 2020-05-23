d0fe901d9b67f6ff2f0d41a4c6e08fe0f3f25483
Bill Adams <wjladams@gmail.com>
2020-05-23 14:58:20 -0400

    Moving thyroid_model.js to config.js
    Will remove thyroid_model.js next commit.

--- html/thyroid-prototype/config.js
+++ html/thyroid-prototype/config.js
@@ -152,3 +152,93 @@ function getResponseMolecularTestName() {
     return "";
   }
 }
+
+
+////////////////////////////////////////////////////////////////////
+//// The AHP Model JSON and creating it                       //////
+////////////////////////////////////////////////////////////////////
+
+var AHP_MODEL_JSON = {
+  "name" : "Thyroid Cancer Model",
+  "description" : "Our first attempt at the Thyroid Cancer Model.",
+  "alts" : ["Observation", "Hemi-Thyroidectomy", "Total Thyroidectomy"],
+  "alt_descriptions" : ["Remain vigilant", "Better explanation", "Remove all of the thyroid"],
+  "children": [
+      {
+        "name": "Not Knowing if I Have Cancer",
+        "id" : 0,
+        "description": "Making Sure I Know if I Have Cancer",
+        "alt_scores": [0, 0.8, 1.0]
+      },
+      {
+        "name" : "Having Surgery",
+        "id" : 1,
+        "description" : "Concerns About Having Surgery",
+        "children" : [
+          {
+            "name": "Having a Scar",
+            "id": 2,
+            "description" : "The different options have differing scarring effects.",
+            "alt_scores" : [1.0, 0.2, 0.1]
+          },
+          {
+            "name": "Potential Low Calcium",
+            "id":3,
+            "description" : "Some options could require you to take a calcium supplement.",
+            "alt_scores" : [1.0, 0.3, 0.1]
+          },
+          {
+            "name": "Voice Complications",
+            "id": 4,
+            "description" : "Some options could effect your voice.",
+            "alt_scores" : [1.0, 0.2, 0.1]
+          },
+          {
+            "name": "Need for Thyroid Supplemenatation",
+            "id":5,
+            "description" : "Some options may require lifelong thyroid medication.",
+            "alt_scores" : [1.0, 0.9, 0.5]
+          },
+          {
+            "name": "Recovery from Surgery",
+            "id":6,
+            "description" : "Some options have longer recovery times.",
+            "alt_scores" : [1.0, 0.9, 0.5]
+          },
+        ],
+        "pairwise" : [
+          [1, 0, 0, 0, 0],
+          [0, 1, 0, 0, 0],
+          [0, 0, 1, 0, 0],
+          [0, 0, 0, 1, 0],
+          [0, 0, 0, 0, 1]
+        ]
+      }
+  ],
+
+  "pairwise": [
+    [1, 0],
+    [0, 1],
+  ],
+
+  "pairwiseOrderByIds": [
+    [2, 3],
+    [3, 4],
+    [4, 5],
+    [5, 6],
+    [2, 4],
+    [2, 5],
+    [2, 6],
+    [3, 5],
+    [3, 6],
+    [4, 6],
+    [0, 1]
+  ]
+
+}
+
+var AHP_MODEL = AHPTreeNode.fromJSONObject(AHP_MODEL_JSON)
+
+////////////////////////////////////////////////////////////////////
+//// End of the AHP Model definition section                  //////
+////////////////////////////////////////////////////////////////////
